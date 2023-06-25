ce963eaa23c7a5bb9fc73a41cfec9524680f7b1a
Minh Mai <phuocminh164@gmail.com>
2023-06-25 09:46:25 +0700

    load elonest commands

--- src/ElonestServiceProvider.php
+++ src/ElonestServiceProvider.php
@@ -3,6 +3,8 @@
 namespace Minh164\EloNest;
 
 use Illuminate\Support\ServiceProvider;
+use Minh164\EloNest\Console\InspectingCommand;
+use Minh164\EloNest\Console\RepairingCommand;
 
 /**
  * Service provider for Elonest package.
@@ -11,6 +13,13 @@ class ElonestServiceProvider extends ServiceProvider
 {
     public function boot(): void
     {
+        if ($this->app->runningInConsole()) {
+            $this->commands([
+                InspectingCommand::class,
+                RepairingCommand::class,
+            ]);
+        }
+
         $this->loadMigrationsFrom(__DIR__ . '/database/migrations');
     }
 }
